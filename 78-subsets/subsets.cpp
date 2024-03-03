6b17984735ff34c0ad52452973fa0a6a8459bb73
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-03-03 18:38:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (60.90%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
 
-/*void helper(int i,vector<int>& subset, vector<int>& nums, vector<vector<int>>& result){
+void helper(int i,vector<int>& subset, vector<int>& nums, vector<vector<int>>& result){
     if(i>=nums.size()){
             result.push_back(subset);
             return;
@@ -12,10 +12,7 @@ public:
     helper(i+1,subset,nums,result);
     subset.pop_back();
     helper(i+1,subset,nums,result);
-helper(i+1,subset,nums,result);
 
-     subset.push_back(nums[i]);
-     helper(i+1,subset,nums,result);
     
 }
     vector<vector<int>> subsets(vector<int>& nums) {
@@ -23,33 +20,8 @@ helper(i+1,subset,nums,result);
         vector<vector<int>> result;
         helper(0,subset,nums,result);
         return result;
-    }*/
-
-void solve(vector<int>& nums, vector<int> temp , int i,vector<vector<int>> &ans) {
-        
-        // BC
-        if(i >= nums.size()){
-            ans.push_back(temp);
-            return ;
-        }
-
-        
-
-        // exclude
-        solve(nums,temp,i+1, ans);
-
-        // include
-        int num = nums[i];
-        temp.push_back(num);
-        solve(nums,temp,i+1,ans);
     }
 
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        int i = 0;
-        solve(nums,temp,i,ans);
-        return ans;
-    }
+
 };
 
