8f409da9727b1d28be686e15a492226bb2d4b74e
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-03-08 17:12:41 +0530

    Time: 11 ms (80.94%) | Memory: 20.4 MB (31.45%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -4,17 +4,11 @@ public:
         int n=nums.size();
         int sum=(n*(n+1))/2;
 int s=0;
-        for(int i=0;i<n;i++){
-s=s+nums[i];
+        for (int i: nums) {
+            s+=i;
         }
 
         return sum-s;
     }
 };
 
-static const int FAST_IO = [](){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}();
