11f136be0d6f52f63de1c116d0773bb99dfce75f
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-03-16 17:45:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (44.26%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -19,8 +19,10 @@ public:
         s.push(root);
         while(!s.empty()){
             TreeNode* temp=s.top();
-            ans.push_back(temp->val);
+            
             s.pop();
+            ans.push_back(temp->val);
+
             if(temp->right!=nullptr) s.push(temp->right);
             
             if(temp->left!=nullptr) s.push(temp->left);
