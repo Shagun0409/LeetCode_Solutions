72bb8be58c13263fda8bb2668bec27a650fe0270
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-03-23 10:53:06 +0530

    Time: 8 ms (59.79%) | Memory: 8.3 MB (48.25%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
-        
-        long rev=0;
-        int digit;
-        int temp=x;
+        if(x<0) return false;
+        long long temp=x;
+        long long rev=0;
         while(x!=0){
-      
-            
-       digit =x%10;
-        
-        rev=digit+rev*10;
-       x=x/10;
+            int last=x%10;
+            x=x/10;
+            rev=(rev*10)+last;
+
         }
-        
-        if(rev==temp){
-            return true;
-        }
-        else{
-            return false;
-        }
+        if(rev==temp) return true;
+        else return false;
         
     }
 };
