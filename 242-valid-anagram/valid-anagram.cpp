4291760525a837956aac863c27db7e21a92a4551
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-03-25 20:23:17 +0530

    Time: 6 ms (80.91%) | Memory: 9.9 MB (5.88%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,20 +2,43 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        map<char,int> m1;
-        map<char,int>m2;
+        /*  map<char,int> m1;
+         map<char,int>m2;
 
-        if(s.length() != t.length()) return false;
-        for(int i=0;i<s.length();i++){
-            m1[s[i]]++;
+         if(s.length() != t.length()) return false;
+         for(int i=0;i<s.length();i++){
+             m1[s[i]]++;
+         }
+
+         for(int i=0;i<t.length();i++){
+             m2[t[i]]++;
+         }
+
+         if(m1==m2) return true;
+         else return false;
+          */
+
+        if (s.length() != t.length()) {
+            return false;
         }
-        
-        for(int i=0;i<t.length();i++){
-            m2[t[i]]++;
+        unordered_map<char, int> mp;
+        for (int i = 0; i < s.length(); i++) {
+            mp[s[i]]++;
         }
 
-        if(m1==m2) return true;
-        else return false;
-        
+        for (int i = 0; i < t.length(); i++) {
+            if (mp.find(t[i]) != mp.end()) {
+                mp[t[i]] -= 1;
+            } else
+                return false;
+        }
+
+        for (auto items : mp) {
+            if (items.second != 0) {
+                return false;
+            } 
+   
+        }
+        return true;
     }
 };
