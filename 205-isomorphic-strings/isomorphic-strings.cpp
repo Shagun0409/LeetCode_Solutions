6748dfe08adfb84fec324a352d83a5ca04ab4d29
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-03-25 23:26:53 +0530

    Time: 6 ms (65.72%) | Memory: 8.2 MB (85.89%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-       int arr1[256]={0};
+      /*  int arr1[256]={0};
        int arr2[256]={0};
 
        for(int i=0;i<s.length();i++) {
@@ -9,6 +9,17 @@ public:
         arr1[s[i]]=i+1;
         arr2[t[i]]=i+1;
        }
-       return true;
+       return true; */
+
+       vector<int>ps(128,-1);
+        vector<int>pt(128,-1);
+        for(int i=0;i<s.size();i++)
+        {
+           if(ps[s[i]]!=pt[t[i]])
+            return 0;
+            ps[s[i]]=i;
+            pt[t[i]]=i;
+        }
+        return 1;
     }
 };
