6e864648c70fc023d8901f3e275dd9cf7a242041
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-04-14 12:53:46 +0530

    Time: 4 ms (46.70%) | Memory: 7.7 MB (56.21%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -3,10 +3,12 @@ public:
     int lengthOfLastWord(string s) {
      int i=s.length()-1;
      int len=0;
+       if(i==0) return 1;
      while(i>=0 && s[i]==' '){
         i--;
      }
      while(i>=0 && s[i]!=' '){
+      
         len++;
         i--;
      }
