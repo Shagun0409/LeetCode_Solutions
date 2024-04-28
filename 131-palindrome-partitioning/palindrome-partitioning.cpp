18c86f75135fd97f6ae3669976ab22d503244b8b
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-04-28 15:25:44 +0530

    Time: 83 ms (70.48%) | Memory: 52.6 MB (93.34%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
-    vector<vector<string>> res;
-    bool pal(int i,int j,string &s)
-    {
-        while(i<j)
-        {
-            if(s[i]!=s[j])
+    bool isPalindrome(const string& s, int low, int high){
+        while(low <= high){
+            if(s[low++] != s[high--]){
                 return false;
-            i++;j--;
-        }
-        return true;
-    }
-    void f(int i,string &s,vector<string> ans)
-    {
-        if(i>=s.size())
-        {
-            if(ans.size()>0)
-               res.push_back(ans);
-            return;
-        }
-        for(int k=i;k<s.size();k++)
-        {
-            if(pal(i,k,s))
-            {
-                ans.push_back(s.substr(i,k-i+1));
-                f(k+1,s,ans);
-                ans.pop_back();
             }
         }
+
+        return true; 
     }
+
+
+    void dfs(int idx,const string& s, vector<vector<string>>& ans,vector<string>& temp){
+        if(idx >= s.size()){
+           ans.push_back(temp);
+           return; 
+        }  
+
+
+        for(int i = idx; i < s.size(); i++){
+           if(isPalindrome(s,idx,i)){
+              temp.push_back(s.substr(idx, i - idx+ 1));
+              dfs(i+1,s,ans,temp);
+              temp.pop_back();
+           }
+        }
+    }
+
+
     vector<vector<string>> partition(string s) {
-        vector<string> ans;
-        f(0,s,ans);
-        return res;
+    
+         vector<vector<string>> ans; 
+
+         vector<string> temp;
+
+          dfs(0,s,ans,temp);
+
+
+          return ans;
+
+        
     }
 };
