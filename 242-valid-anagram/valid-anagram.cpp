6ce3339e9bc860d3bd8db7027dedc0744c022fc7
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-06-06 16:24:35 +0530

    Time: 12 ms (29.66%) | Memory: 9.7 MB (19.50%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
-        /*  map<char,int> m1;
+         map<char,int> m1;
          map<char,int>m2;
 
          if(s.length() != t.length()) return false;
@@ -15,30 +14,6 @@ public:
          }
 
          if(m1==m2) return true;
-         else return false;
-          */
-
-        if (s.length() != t.length()) {
-            return false;
-        }
-        unordered_map<char, int> mp;
-        for (int i = 0; i < s.length(); i++) {
-            mp[s[i]]++;
-        }
-
-        for (int i = 0; i < t.length(); i++) {
-            if (mp.find(t[i]) != mp.end()) {
-                mp[t[i]] -= 1;
-            } else
-                return false;
-        }
-
-        for (auto items : mp) {
-            if (items.second != 0) {
-                return false;
-            } 
-   
-        }
-        return true;
+         else return false;        
     }
 };
