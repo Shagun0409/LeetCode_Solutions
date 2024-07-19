520a4cb79430df19063e598aca5565f9fb3305b7
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2024-07-19 11:58:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (10.97%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -6,31 +6,31 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
+    vector<vector<int>> v;
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-       vector<vector<int>> v;
-        if(root==NULL) return v;
-        queue<TreeNode*>q;
+        if(!root) return v;
+        queue<TreeNode*> q;
         q.push(root);
-        bool leftToRight = true;
-        while(!q.empty()){
-            int size = q.size();
-            vector<int> ans(size);
-            for(int i = 0;i<size;i++){
-                TreeNode*frontNode = q.front();
+        
+        while(q.size()>0){
+            vector<int> ans;
+            int l=q.size();
+            for(int i=0;i<l;i++){
+                TreeNode* temp=q.front();
                 q.pop();
-                int index = leftToRight ? i : size-i-1;
-                ans[index] = frontNode->val;
-                if(frontNode->left) q.push(frontNode->left);
-                if(frontNode->right) q.push(frontNode->right);
+                ans.push_back(temp->val);
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
             }
-            leftToRight = !leftToRight;
             v.push_back(ans);
+        }
+        for(int i=0;i<v.size();i++){
+            if(i%2==1) reverse(v[i].begin(),v[i].end());
         }
         return v;
     }
