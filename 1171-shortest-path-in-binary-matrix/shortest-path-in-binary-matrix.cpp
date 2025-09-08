29f6964f4e8d0cb0f3f6ce73d32387aa14db6c05
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2025-09-08 23:58:48 +0530

    Time: 11 ms (90.35%) | Memory: 24.9 MB (64.56%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+            return -1;
+        if (n == 1)
+            return 1;
+        vector<pair<int, int>> dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
+                                       {0, 1},   {1, -1}, {1, 0},  {1, 1}};
+        queue<array<int, 3>> q;
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        q.push({0, 0, 1});
+        visited[0][0] = true;
+
+        while (!q.empty()) {
+            auto [r, c, dist] = q.front();
+            q.pop();
+            if (r == n - 1 && c == n - 1)
+                return dist;
+
+            for (auto [dr, dc] : dirs) {
+                int nr = r + dr, nc = c + dc;
+                // Check bounds, open cell, not visited
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n &&
+                    grid[nr][nc] == 0 && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    q.push({nr, nc, dist + 1});
+                }
+            }
+        }
+        return -1;
+    }
+};
