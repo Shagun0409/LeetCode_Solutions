099bb3a8ccbab9d7583ae7d26f6b364494753679
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2025-09-15 21:36:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (90.37%) - LeetSync

---
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+
+     bool mp[26];
+
+     for(char &ch:brokenLetters){
+        mp[ch-'a']=true;
+     }
+
+     int result=0;
+
+
+     bool cantype=true;
+
+     for(char &ch:text){
+        if(ch==' ' ){
+            if(cantype){
+                result++;
+            }
+            cantype=true;
+        }
+        else if(mp[ch-'a']==true){
+            cantype=false;
+
+        }
+     }
+     if(cantype){
+        result++;
+     }
+     return result;
+        
+    }
+};
