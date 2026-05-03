963085716368de464e870f6eb2513ea4ff5680b1
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2026-05-03 13:21:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (45.12%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for(int i=0;i<s.length();i=i+2*k){
+            int j=min(i+k,(int)s.length());
+
+            reverse(begin(s)+i,begin(s)+j);
+        }
+        return s;
+        
+    }
+};
