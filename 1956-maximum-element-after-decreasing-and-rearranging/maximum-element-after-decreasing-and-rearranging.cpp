8d355f073300c475ba4580b48340a82795b8362c
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2026-06-28 13:16:13 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (8.47%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -8,18 +8,10 @@ public:
             }
 
         for(int i=1;i<n;i++){
-            
 
-            int curr1=arr[i-1]+1;
-            int curr2=arr[i-1];
-
-            if( abs(arr[i]-arr[i-1])<=1){
-                continue;
-            }else{
-                arr[i]=min(arr[i],arr[i-1]+1);
-            }
+             arr[i] = min(arr[i], arr[i - 1] + 1);
         }
 
-        return *max_element(arr.begin(), arr.end());
+        return arr.back();
     }
 };
