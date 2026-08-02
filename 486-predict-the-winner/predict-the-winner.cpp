ecc06ffa601c02855db7a40d2d8247cd188fb5e5
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2026-08-02 12:22:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (32.41%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,7 +1,9 @@
 class Solution {
 
 public:
+vector<vector<int>> dp;
     int solve(int i, int j, vector<int>& nums) {
+        
          if (i > j) {
             return 0;
         }
@@ -9,13 +11,18 @@ public:
         if (i == j) {
             return nums[i];
         }
+        if (dp[i][j] != -1)
+            return dp[i][j];
         int ti = nums[i] + min(solve(i + 2, j,nums), solve(i + 1, j - 1,nums));
 
         int tj = nums[j] + min(solve(i, j - 2,nums), solve(i + 1, j - 1,nums));
-        return max(ti,tj);
+        return dp[i][j] = max(ti, tj);
     }
     bool predictTheWinner(vector<int>& nums) {
+  
+        
         int n = nums.size();
+         dp = vector<vector<int>>(n, vector<int>(n, -1));
         int t = 0;
         for (int i = 0; i < n; i++) {
     t += nums[i];
