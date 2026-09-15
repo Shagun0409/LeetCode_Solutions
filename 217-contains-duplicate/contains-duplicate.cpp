8bcf536274d3750f83a6f2aa59d7a5ea00c5702c
Shagun Goyal <110756704+Shagun0409@users.noreply.github.com>
2026-09-15 23:43:42 +0530

    Time: 100 ms (11.71%) | Memory: 113.2 MB (23.08%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        unordered_map<int,int>mp;
+
+        for(auto it: nums){
+            mp[it]++;
         }
+
         for(auto it:mp){
-            if(it.second>1) return true;
+            if(it.second!=1){
+                return true;
+         
+            
+            }
         }
-        
-        return false;
+
+ return false;
     }
+   
+        
+    
 };
